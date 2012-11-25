e2a27054cd2991ebbf120f36dde648d649d1df70
Borisov Dmitry <ya.whiteguy@yandex.ru>
2012-11-26 00:41:43 +0600

    add event to table logic

--- event-controller.js
+++ event-controller.js
@@ -2,21 +2,53 @@ function $(id) {
 	return document.getElementById(id);
 }
 
-function addEventRow() {
-	var table = $('eventTable');
-	table.innerHTML += '<tr>'+
-							'<td>Имя</td>'+
-							'<td>Место23423423423</td>'+
-							'<td>Начало234234234234</td>'+
-							'<td>Конец234234234234234</td>'+
-							'<td>Периодичность</td>'+
-							'<td>Предупреждение</td>'+
-							'<td>Заметки</td>'+
-						'</tr>';
+function initSelect(select, obj) {
+	var item, index = 0;
+	for (item in obj) {
+		if (obj.hasOwnProperty(item)) {
+			select.innerHTML += '<option value="' + item + '">' + obj[item].title + '</option>';
+		}
+	}
 }
 
-function addClick(e) {
-	title = $('title').value;
-	addEventRow();
-	return false;
+function initControls() {
+	initSelect($('repeat'), Const.REPEAT);
+	initSelect($('alert'), Const.ALERT);
+}
+
+function addEventRow(event) {
+	var tbody = $('eventTable').getElementsByTagName("tbody")[0], evRowHTML = '<tr>', prop;
+	for (prop in event) {
+		if (event.hasOwnProperty(prop)) {
+			evRowHTML += '<td>' + event[prop] + '</td>';
+		}
+	}
+	tbody.innerHTML += evRowHTML + '</tr>';
+}
+
+function invalidateForm(validator) {
+
+}
+
+function addClick() {
+	var validator, event = new Event({
+		title : $('title').value,
+		location : $('location').value,
+		startDate : new Date($('startDate').value),
+		endDate : new Date($('endDate').value),
+		repeat : Const.REPEAT[$('repeat').value],
+		alert : Const.ALERT[$('alert').value],
+		notes : $('notes').value
+	});
+	validator = event.validate();
+	if (validator.valid) {
+		addEventRow(event);
+	} else {
+		invalidateForm(validator);
+	}
+}
+
+function clearTable() {
+	var tbody = $('eventTable').getElementsByTagName("tbody")[0];
+	tbody.innerHTML = "";
 }
